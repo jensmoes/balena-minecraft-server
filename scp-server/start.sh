d8b43dc6b32ff8e1e3e1d586bd7069fdbbe2529c
Jens Troest <jens.troest@vidaloop.com>
2020-10-10 14:56:10 -0700

    Add support for SSH access to SCP server. Disable password when using SSH

--- scp-server/start.sh
+++ scp-server/start.sh
@@ -4,10 +4,20 @@ if [[ -z "$SCP_PASSWORD" ]]; then
   export SCP_PASSWORD=balena
 fi
 
+if [[ ! -z "SSH_PUBLIC_KEY" ]]; then
+  mkdir -p ~/.ssh
+  chmod 700 .ssh
+  echo ${SSH_PUBLIC_KEY} | cat >> ~/.ssh/authorized_keys
+  chmod 600 ~/.ssh/authorized_keys
+fi
+
 # here we set up the config for openSSH.
 mkdir /var/run/sshd
 echo "root:$SCP_PASSWORD" | chpasswd
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+# Enable password unless we are using SSH
+if [[ -z "SSH_PUBLIC_KEY" ]]; then
+  sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+fi
 
 # SSH login fix. Otherwise user is kicked off after login. 
 # Apparently not needed with balena image
